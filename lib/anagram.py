3eb6afe3b7c3ef3a3364accb642e9001cd20e8a3
Harriet Chepngeno <harrietseroney2023@gmail.com>
2025-06-17 03:05:07 +0300

    utilising sort method in oop test

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,23 @@
-# your code goes here!
+anagram_words = [
+    "silent",
+    "dust",
+    "heart",
+    "listen",
+    "night",
+    "earth",
+    "thing",
+    "stud",
+    "evil",
+    "live"
+]
+
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+
+    def match (self, words_list):
+        sorted_word = sorted(self.word.lower())
+        return [word for word in words_list if sorted(word.lower())== sorted_word and word.lower() != self.word.lower()]
+
+find_words = Anagram("Listen")
+print(find_words.match(anagram_words))
